a859c6aff76a10757dbbc10f4a25ce64b1e38968
IX <borntokill.ix@gmail.com>
2024-12-26 00:23:50 +0700

    ref: add get_email_settings to __init__.py

--- app/configs/__init__.py
+++ app/configs/__init__.py
@@ -1,2 +1,2 @@
 from .log_settings import LogConfig, get_logger
-from .settings import get_appsettings, get_database_settings
+from .settings import get_appsettings, get_database_settings, get_email_settings
