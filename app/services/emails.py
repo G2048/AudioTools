f5a6d4d921d238de46166c5191c90f2adba8da6a
IX <borntokill.ix@gmail.com>
2024-12-26 01:47:30 +0700

    feat(emails): add class for sending email via smtp

--- app/services/emails.py
+++ app/services/emails.py
@@ -1,5 +1,9 @@
 import logging
+import smtplib
+import ssl
 from collections.abc import Sequence
+
+from app.configs.settings import EmailSettings
 
 # from email.message import EmailMessage
 # from email.mime.multipart import MIMEMultipart
@@ -7,9 +11,13 @@ from collections.abc import Sequence
 # from email.utils import formataddr
 
 logger = logging.getLogger("stdout")
+# settings = get_email_settings()
+# logger.debug(f"Email sender settings: {settings}")
 
 
 class Email:
+    __slots__ = ("email", "message")
+
     def __init__(self, email: str, message: str):
         self.email = email
         self.message = message
@@ -22,8 +30,40 @@ class Email:
 
 
 class EmailSender:
-    def __init__(self):
+    def __init__(self, settings: EmailSettings):
+        self.port = settings.port
+        self.host = settings.host
+        self.password = settings.password
+        self.sender = settings.sender
+
+    def connect(self):
+        server = smtplib.SMTP(self.host, self.port)
+        _context = ssl.create_default_context()
+        server.starttls(context=_context)
+        server.login(self.sender, self.password)
+        return server
+
+    def quit(self):
         pass
 
-    def execute(self, emails: Sequence[str]):
+    def __repr__(self):
+        return self.__str__()
+
+    def send(self, email: Email):
+        server = self.connect()
+        try:
+            server.sendmail(self.sender, email.email, email.message)
+        finally:
+            server.quit()
+
+    def send_batch(self, emails: Sequence[Email]):
+        """
+        Sends emails in a batch
+        :param emails: list of emails
+        :return:
+        """
         pass
+
+    def execute(self, emails: Sequence[Email]):
+        """For the Action interface compliance"""
+        self.send_batch(emails)
