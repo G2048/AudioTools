d0620093a53df9acc4e910e62d260a4f795d7a8c
IX <borntokill.ix@gmail.com>
2024-12-29 16:35:41 +0700

    feat(audio): upload file

--- app/services/audio.py
+++ app/services/audio.py
@@ -2,24 +2,86 @@ import logging
 
 # import ffmpy
 import numpy as np
+from ffmpy import FFmpeg
 from transformers import Pipeline, pipeline
+
+from app.configs import get_aws_bucket_settings, get_aws_settings
+
+from .uploader import AwsUploader
 
 logger = logging.getLogger("stdout")
 
-# ffmpy.FFmpeg.run(
-# inputs={"-i": "pipe:0", "-c:a": "copy", "-f": "mp3", "-"},
-# outputs={"pipe:1": "pipe:1"},
-# global_options={"-loglevel": "quiet"},
-# )
+
+aws_settings = get_aws_settings()
+bucket_settings = get_aws_bucket_settings()
 
 
-# TODO: Тут должна быть логика загрузки аудио в Yandex Storage
 class AudioUploader:
-    def __init__(self):
-        pass
+    __slot__ = "uploader"
 
-    def execute(self, audio: np.ndarray):
-        pass
+    def __init__(self):
+        self.uploader = AwsUploader(aws_settings, bucket_settings)
+
+    def execute(self, file_path: str):
+        logger.info(f"Uploading {file_path}")
+        # Узкое место: если в пути будет несколько директорий,
+        # то будет браться только первая - это плохо, но пока не понятно нужно ли большее
+        current_path = "." if not len(file_path.split("/")) > 1 else file_path.split("/")[0]
+        file = self.uploader.create_file(file_path)
+        self.uploader.upload(file, current_path)
+
+
+class AudioFiles:
+    list_audio = {}
+    __slot__ = ("list_audio", "file")
+
+    def __init__(self, file: str):
+        self.file = file.split(".")[0]
+
+    def convert_from_numpy(self, audio: np.ndarray):
+        return
+
+    def create(self, format: str) -> dict:
+        formated_file = f"{self.file}.{format}"
+        self.list_audio.update({formated_file: None})
+        return self.list_audio
+
+    @classmethod
+    def clean(cls):
+        cls.list_audio.clear()
+
+
+class AudioConverter:
+    def __init__(self, name: str, output_file: AudioFiles):
+        self.file = output_file
+        self.name = name
+
+    def convert_mp3(self):
+        self.convert()
+
+    def convert_ogg(self):
+        self.convert("ogg")
+
+    def convert_wav(self):
+        self.convert("wav")
+
+    # TODO: add capture of stderr !
+    def convert(self, format="mp3"):
+        logger.info(f"Converting {self.file} to {format}")
+        ff = FFmpeg(inputs={self.name: None}, outputs=self.file.create(format))
+
+        logger.debug(f"String for execution: {ff.cmd}")
+        try:
+            ff.run()
+            logger.error(f"Error while converting {self.file} to {format}")
+        except Exception:
+            logger.error(f"String for execution: {ff.cmd}")
+
+    def to_format(self, audio: np.ndarray):
+        return audio.convert_from_numpy(audio)
+
+    # def _to_format(self, audio: np.ndarray):
+    #     return {f"{self.file}.{self.format}": None}
 
 
 class AudioRecognizer:
