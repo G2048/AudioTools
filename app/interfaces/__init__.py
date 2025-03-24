a268358bc314ee42f445be2f0fe8e9b57db6d153
IX <borntokill.ix@gmail.com>
2025-03-24 22:22:53 +0700

    feat(interfaces): highlight the interface for recognition the RecognizerInterface
    split audio interfaces and recognize interfaces
    
    BREAKING CHANGE:

--- app/interfaces/__init__.py
+++ app/interfaces/__init__.py
@@ -2,5 +2,5 @@ from .audio import AudioUploaderInterface, RecognizedText, SpeechRecognizerInter
 from .files import FileInterface, UploaderInterface
 from .iam import ImTokenInterface
 from .pages import Page
-from .recognizers import AudioRecognizerInterface
+from .recognizers import RecognizedText, RecognizedTextInterface, RecognizedTexts, RecognizerInterface
 from .senders import SenderInterface
--- app/interfaces/recognizers.py
+++ app/interfaces/recognizers.py
@@ -1,12 +1,52 @@
 from abc import ABC, abstractmethod
+from typing import BinaryIO, TypeAlias
 
-import numpy as np
+from pydantic import BaseModel
 
-RecognizedText = str
+start_time: TypeAlias = str
+end_time: TypeAlias = str
 
 
-class AudioRecognizerInterface(ABC):
+class RecognizedText(BaseModel):
+    timestamps: tuple[start_time, end_time]
+    text: str
+
+
+class RecognizedTexts(BaseModel):
+    chunk_texts: list[RecognizedText]
+
+
+# {
+#     text: "some text",
+#     timestamps: ("00:00:00", "00:00:01"),
+# }
+
+
+class RecognizedTextInterface(ABC):
     @abstractmethod
-    # def execute(self, audio_file: str, channels_count: int) -> RecognizedText:
-    def recognize(self, audio: np.ndarray) -> RecognizedText:
+    def get_ready_text(self) -> RecognizedTexts:
+        pass
+
+
+task_id: TypeAlias = str
+file_id: TypeAlias = str
+status: TypeAlias = str
+
+
+class RecognizerInterface(ABC):
+    @property
+    @abstractmethod
+    def name(self) -> str:
+        pass
+
+    @abstractmethod
+    def recognize(self, audio_file: BinaryIO) -> task_id:
+        pass
+
+    @abstractmethod
+    def check_status(self, task_id: str) -> dict[status, file_id]:
+        pass
+
+    @abstractmethod
+    def download_file(self, file_id: str) -> RecognizedTextInterface:
         pass
