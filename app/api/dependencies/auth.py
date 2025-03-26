b5fd3c16d4d2bb71432a7c9a07cca8d78ea0a0c2
IX <borntokill.ix@gmail.com>
2025-03-26 22:58:28 +0700

    feat(api/dependencies): add the check_auth function for validation of JWT token

---
+++ app/api/dependencies/auth.py
@@ -0,0 +1,46 @@
+import logging
+from http import HTTPStatus
+from typing import Annotated
+
+from fastapi import Depends, HTTPException
+from fastapi.responses import JSONResponse
+from fastapi.security import OAuth2PasswordBearer
+
+from app.services.jwt import JWT, ExpiredSignatureError, InvalidTokenError
+
+security = OAuth2PasswordBearer(tokenUrl="/api/v1/login/")
+
+logger = logging.getLogger("stdout")
+
+jwt = JWT()
+
+
+def check_auth(token: Annotated[str, Depends(security)]):
+    logger.debug(f"Token: {token=}")
+
+    logged_in = False
+    status_code = HTTPStatus.UNAUTHORIZED
+    error = False
+    if token:
+        try:
+            jwt.validate(token)
+        except ExpiredSignatureError:
+            logger.info(f"Token expired: {jwt.payload(token)=}")
+            detail = "Token expired"
+            error = True
+        except InvalidTokenError:
+            logger.info(f"Token invalid: {token=}")
+            detail = "Invalid token"
+            error = True
+
+        if error:
+            raise HTTPException(
+                status_code=status_code,
+                detail=detail,
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+        else:
+            logged_in = True
+            status_code = HTTPStatus.OK
+
+    return JSONResponse(content={"logged_in": logged_in}, status_code=status_code)
