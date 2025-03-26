c1c2184f0dbed681944503b52f4f6cc034ac6c00
IX <borntokill.ix@gmail.com>
2025-03-26 22:37:04 +0700

    fix(adapters/recognizers): add _audio_from_file for converting audio_file to np.ndarrray and get sample rate of audio

--- app/adapters/recognizers/neural.py
+++ app/adapters/recognizers/neural.py
@@ -1,9 +1,11 @@
 import logging
 import threading
 import uuid
+from pathlib import Path
 from typing import BinaryIO
 
 import numpy as np
+from pydub import AudioSegment
 from transformers import Pipeline, pipeline
 
 from app.configs import get_neural_settings
@@ -28,7 +30,7 @@ class NeuralRecognizedText(RecognizedTextInterface):
     def get_ready_text(self) -> RecognizedTexts:
         processing_text = [
             RecognizedText(
-                timestamps=(chunk["timestamp"][0], chunk["timestamp"][1]),
+                timestamps=(str(chunk["timestamp"][0]), str(chunk["timestamp"][1])),
                 text=chunk["text"],
             )
             for chunk in self.chunks
@@ -37,7 +39,7 @@ class NeuralRecognizedText(RecognizedTextInterface):
 
 
 class WhisperRecognizer(RecognizerInterface):
-    transcriber: Pipeline = pipeline(
+    __transcriber: Pipeline = pipeline(
         "automatic-speech-recognition", max_new_tokens=445, model=neural_settings.name
     )
     _TASKS = {}
@@ -58,8 +60,8 @@ class WhisperRecognizer(RecognizerInterface):
         self._TASKS[task_id] = {"status": Status.PROCESSING, "file_id": self._create_file_id()}
         logger.info(f"Create task for Neural {self.name}: {task_id}")
 
-        audio_array = self.binay_io_to_numpy(audio_file)
-        thread_task = threading.Thread(target=self.transcribe, args=(audio_array, task_id), daemon=True)
+        # audio_array = self.binay_io_to_numpy(audio_file)
+        thread_task = threading.Thread(target=self._transcribe, args=(audio_file.name, task_id), daemon=True)
         thread_task.start()
         return task_id
 
@@ -75,11 +77,32 @@ class WhisperRecognizer(RecognizerInterface):
     def binay_io_to_numpy(binary_io: BinaryIO) -> np.ndarray:
         return np.frombuffer(binary_io.read(), dtype=np.uint8)
 
-    def transcribe(self, audio: np.ndarray, task_id: str):
-        assert isinstance(audio, np.ndarray)
-        print(f"Transcribe audio:{audio=}")
-        print(f"Instanse audio:{type(audio)=}")
-        sr, y = audio
+    @staticmethod
+    def _audio_from_file(filename: str, crop_min: float = 0, crop_max: float = 100) -> tuple[int, np.ndarray]:
+        try:
+            audio = AudioSegment.from_file(filename)
+        except FileNotFoundError as e:
+            isfile = Path(filename).is_file()
+            msg = (
+                f"Cannot load audio from file: `{'ffprobe' if isfile else filename}` not found."
+                + " Please install `ffmpeg` in your system to use non-WAV audio file formats"
+                " and make sure `ffprobe` is in your PATH."
+                if isfile
+                else ""
+            )
+            raise RuntimeError(msg) from e
+        if crop_min != 0 or crop_max != 100:
+            audio_start = len(audio) * crop_min / 100
+            audio_end = len(audio) * crop_max / 100
+            audio = audio[audio_start:audio_end]
+        data = np.array(audio.get_array_of_samples())
+        if audio.channels > 1:
+            data = data.reshape(-1, audio.channels)
+        return audio.frame_rate, data
+
+    def _transcribe(self, audio_file: str, task_id: str):
+        print(f"Transcribe audio:{audio_file=}")
+        sr, y = self._audio_from_file(audio_file)
         logger.info(f"Audio size: {y.shape}")
         logger.info(f"Audio sampling rate: {sr}")
 
@@ -90,7 +113,7 @@ class WhisperRecognizer(RecognizerInterface):
         y = y.astype(np.float32)
         y /= np.max(np.abs(y))
         try:
-            transcribed_text = self.transcriber({"sampling_rate": sr, "raw": y}, return_timestamps=True)
+            transcribed_text = self.__transcriber({"sampling_rate": sr, "raw": y}, return_timestamps=True)
         except Exception as e:
             self._TASKS[task_id]["status"] = Status.ERROR
             logger.error(f"Error While transcribing: {e}")
@@ -98,13 +121,3 @@ class WhisperRecognizer(RecognizerInterface):
         else:
             self._TASKS[task_id]["status"] = Status.SUCCESS
             self._TASKS[task_id]["text"] = transcribed_text
-
-    # def recognize(self, audio: np.ndarray) -> str:
-    #     transcribed = self.transcribe(audio)
-    #     logger.debug(f"Transcribed: {transcribed}")
-    #     chunks: list[dict[str, str]] = transcribed["chunks"]
-    #     processing_text = ""
-    #     for chunk in chunks:
-    #         processing_text += f"{chunk['timestamp'][0]} - {chunk['timestamp'][1]}: {chunk['text']}\n"
-
-    #     return processing_text
