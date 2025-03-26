628486aa20c3bf55004808125f505ed31ff28eb7
IX <borntokill.ix@gmail.com>
2025-03-26 22:59:33 +0700

    refactor(v1/routers): change check_login to Dependencies check_auth in /login

--- app/api/v1/routers/login.py
+++ app/api/v1/routers/login.py
@@ -3,11 +3,11 @@ from http import HTTPStatus
 from typing import Annotated
 
 from fastapi import APIRouter, Depends, HTTPException
-from fastapi.responses import JSONResponse
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestFormStrict
 
+from app.api.dependencies.auth import check_auth
 from app.api.models.users import Token
-from app.services.jwt import JWT, ExpiredSignatureError, InvalidTokenError
+from app.services.jwt import JWT
 
 router = APIRouter(
     prefix="/api/v1/login",
@@ -17,7 +17,7 @@ logger = logging.getLogger("stdout")
 
 
 # TODO: add check logick for authentication
-def check_auth(username: str, password: str) -> bool:
+def registre_user(username: str, password: str) -> bool:
     return True
 
 
@@ -28,7 +28,7 @@ security = OAuth2PasswordBearer(tokenUrl="/api/v1/login/")
 def login(body: Annotated[OAuth2PasswordRequestFormStrict, Depends()]) -> Token:
     logger.debug(f"Body: {body=}")
     username = body.username.partition("@")[0]
-    logged_in = check_auth(username, body.password)
+    logged_in = registre_user(username, body.password)
     logger.info(f"Username: {username=}")
     if not logged_in:
         raise HTTPException(
@@ -40,33 +40,5 @@ def login(body: Annotated[OAuth2PasswordRequestFormStrict, Depends()]) -> Token:
 
 
 @router.get("/")
-def check_login(token: Annotated[str, Depends(security)]):
-    logger.debug(f"Token: {token=}")
-    jwt = JWT()
-
-    logged_in = False
-    status_code = HTTPStatus.UNAUTHORIZED
-    error = False
-    if token:
-        try:
-            jwt_token = jwt.validate(token)
-        except ExpiredSignatureError:
-            logger.info(f"Token expired: {jwt.payload(token)=}")
-            detail = "Token expired"
-            error = True
-        except InvalidTokenError:
-            logger.info(f"Token invalid: {token=}")
-            detail = "Invalid token"
-            error = True
-
-        if error:
-            raise HTTPException(
-                status_code=status_code,
-                detail=detail,
-                headers={"WWW-Authenticate": "Bearer"},
-            )
-        else:
-            logged_in = True
-            status_code = HTTPStatus.OK
-
-    return JSONResponse(content={"logged_in": logged_in}, status_code=status_code)
+def check_login(token: Annotated[str, Depends(check_auth)]):
+    return token
