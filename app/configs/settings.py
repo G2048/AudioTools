90bf81720f491fc6ef31474ad17a9b56124fb151
IX <borntokill.ix@gmail.com>
2025-03-28 18:17:17 +0700

    fix(settings): do Optional the object_path and directory_path of AwsBucketSettingsConfig class

--- app/configs/settings.py
+++ app/configs/settings.py
@@ -57,8 +57,8 @@ class AwsBucketSettingsConfig(BaseSettings):
     model_config = SettingsConfigDict(env_prefix="AWS_")
 
     bucket_name: str
-    object_path: Optional[str] = None
-    directory_path: Optional[str] = None
+    object_path: Optional[str] = ""
+    directory_path: Optional[str] = ""
 
 
 class NeuralSettings(BaseSettings):
