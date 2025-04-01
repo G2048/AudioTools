30bddccbe12ba728a9e3a95ebca832cf3ab45ce7
IX <borntokill.ix@gmail.com>
2025-04-01 21:48:44 +0700

    refactor(services/jwt): change input for JWT.generate_token() to JwtPayload model
    move on calculate exp and iat to computed_filed of JwtPayload; token_ttl is Optional field
    
    BREAKING CHANGE:

--- app/services/jwt.py
+++ app/services/jwt.py
@@ -2,15 +2,25 @@ from datetime import datetime, timedelta
 
 import jwt
 from jwt import ExpiredSignatureError, InvalidTokenError
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict, computed_field
 
 from app.configs.settings import JwtSettings
 
 
 class JwtPayload(BaseModel):
-    exp: int
-    iat: int
+    model_config = ConfigDict(extra="allow")
+
     sub: str
+    token_ttl: int = 20
+
+    @computed_field(return_type=int)
+    def exp(self):
+        expiration_time = datetime.now() + timedelta(days=self.token_ttl)
+        return int(expiration_time.timestamp())
+
+    @computed_field(return_type=int)
+    def iat(self):
+        return int(datetime.now().timestamp())
 
 
 class JWT:
@@ -19,16 +29,8 @@ class JWT:
     SECRET_KEY = _jwt_settings.secret_key
     ALGORITHM = _jwt_settings.algorithm
 
-    def generate_token(self, data: dict, live_days: int = 20) -> str:
-        expiration_time = datetime.now() + timedelta(days=live_days)
-        payload = JwtPayload(
-            exp=int(expiration_time.timestamp()),
-            iat=int(datetime.now().timestamp()),
-            sub=data["username"],
-        )
-
-        token = jwt.encode(payload.model_dump(), self.SECRET_KEY, algorithm=self.ALGORITHM)
-        return token
+    def generate_token(self, payload: JwtPayload) -> str:
+        return jwt.encode(payload.model_dump(), self.SECRET_KEY, algorithm=self.ALGORITHM)
 
     def validate(self, token: str) -> JwtPayload:
         _jwt = jwt.decode(token, self.SECRET_KEY, algorithms=[self.ALGORITHM])
