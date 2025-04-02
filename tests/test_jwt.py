c7bb8f4d793a00135a6cd7bebd8cc0b5a3710498
IX <borntokill.ix@gmail.com>
2025-04-02 21:02:33 +0700

    test(services/jwt): add the unittest TestJwt class

---
+++ tests/test_jwt.py
@@ -0,0 +1,32 @@
+import unittest
+
+from app.services.jwt import JWT, JwtPayload
+
+
+class TestJwt(unittest.TestCase):
+    def setUp(self) -> None:
+        self.client = JWT
+
+    def tearDown(self) -> None:
+        return super().tearDown()
+
+    def test_generate_token(self):
+        test_token = self.client.generate_token(JwtPayload(sub="test"))
+        self.assertIsNotNone(test_token)
+        self.assertIsInstance(test_token, str)
+        print(f"{test_token=}")
+
+    def test_validate(self):
+        test_token = self.client.generate_token(JwtPayload(sub="test"))
+        token_model = self.client.validate(test_token)
+        self.assertIsNotNone(token_model)
+        self.assertIsInstance(token_model, JwtPayload)
+        print(f"{token_model=}")
+
+    def test_payload(self):
+        test_token = self.client.generate_token(JwtPayload(sub="test"))
+        token_model = self.client.payload(test_token)
+        self.assertIsNotNone(token_model)
+        self.assertIsInstance(token_model, JwtPayload)
+        self.assertEqual(token_model.sub, "test")
+        print(f"{token_model=}")
