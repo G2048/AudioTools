c7d47ae528f5f164851bae2e5473d9f7c75081e1
IX <borntokill.ix@gmail.com>
2025-05-23 23:55:51 +0700

    feat(app/adapters/converters/ffmpeg.py): add realization for Mp3Converter, Mp3Converter and OggConverter classes implementing the IAudioConverter interface

--- app/adapters/converters/ffmpeg.py
+++ app/adapters/converters/ffmpeg.py
@@ -2,6 +2,7 @@ import logging
 from typing import BinaryIO
 
 from app.interfaces.audio import IAudioConverter
+from app.services.audio import FFmpegConverter
 
 logger = logging.getLogger("app.adapters.converters")
 
@@ -10,18 +11,24 @@ class Mp3Converter(IAudioConverter):
     __slots__ = ()
 
     def convert(self, file: BinaryIO) -> str:
-        return ""
+        converter = FFmpegConverter(file.name)
+        segment = converter.convert_mp3()
+        return segment.name
 
 
 class WavConverter(IAudioConverter):
     __slots__ = ()
 
     def convert(self, file: BinaryIO) -> str:
-        return ""
+        converter = FFmpegConverter(file.name)
+        segment = converter.convert_wav()
+        return segment.name
 
 
 class OggConverter(IAudioConverter):
     __slots__ = ()
 
     def convert(self, file: BinaryIO) -> str:
-        return ""
+        converter = FFmpegConverter(file.name)
+        segment = converter.convert_ogg()
+        return segment.name
