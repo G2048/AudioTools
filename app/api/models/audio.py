3f0fbcac405680d74a147e5655b4d5e2628f628c
IX <borntokill.ix@gmail.com>
2025-05-24 13:24:31 +0700

    fix(app/api/models/audio.py): add ResponseStatus, ResponseAvailableRecognizers and ResponseTaskId for api responses

---
+++ app/api/models/audio.py
@@ -0,0 +1,23 @@
+from typing import Optional
+
+from pydantic import BaseModel
+
+from app.interfaces.recognizers import File_id, Task_id
+
+
+class Audio(BaseModel):
+    status: str
+    file_id: File_id
+
+
+class ResponseStatus(BaseModel):
+    status: str
+    file_id: Optional[File_id] = None
+
+
+class ResponseAvailableRecognizers(BaseModel):
+    providers: list[str]
+
+
+class ResponseTaskId(BaseModel):
+    task_id: Task_id
