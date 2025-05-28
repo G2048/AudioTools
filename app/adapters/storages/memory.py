285283b664915fce1c87a507cdb3d1b98c70969b
IX <borntokill.ix@gmail.com>
2025-05-28 22:03:37 +0700

    refactor(app/adapters/storages/memory.py): change the RedisTaskStorage class from new app.drivers.ipc.redis package

--- app/adapters/storages/memory.py
+++ app/adapters/storages/memory.py
@@ -1,8 +1,7 @@
-import json
 import logging
 
 from app.configs.settings import get_redis_ipc_settings
-from app.drivers.ipc.redis_ipc import RedisException, RedisIPC, RedisMessage
+from app.drivers.ipc.redis import HashStore, RedisConnection, RedisException
 from app.interfaces.storages import ITaskStorage, Task_id, TaskMessage, TTLTaskId
 
 logger = logging.getLogger("app.adapters.storages")
@@ -18,27 +17,43 @@ class TaskIdNotFoundError(StorageException):
 
 
 class RedisTaskStorage(ITaskStorage):
+    TASKS_KEY = "TASKS_ID"
+    TASK_TTL = TTLTaskId
+
     def __init__(self):
         try:
-            self.redis = RedisIPC(get_redis_ipc_settings())
-            self.redis.ping()
+            config = get_redis_ipc_settings()
+            self.store = HashStore(RedisConnection(config))
         except RedisException as e:
             raise StorageException(e.detail)
 
     def set(self, message: TaskMessage) -> bool:
         logger.info(f"Set Task_id: {message.task_id} -> {message.recognizer}")
-        return self.redis.setx(
-            RedisMessage(
-                key=message.task_id, value=message.model_dump_json(), ttl=TTLTaskId
-            )
-        )
+        success = self.store.set(message.task_id, mapping=message.model_dump())
+        self.store.set_key_ttl(message.task_id, self.TASK_TTL)
+
+        ttl = self.store.ttl(message.task_id)
+        logger.info(f"TTL Task_id: {message.task_id} -> {ttl}")
+
+        logger.info(f"Write {message.task_id} to {self.TASKS_KEY}")
+
+        self.store.set(self.TASKS_KEY, {message.task_id: message.task_id})
+        self.store.set_fttl(self.TASKS_KEY, message.task_id, self.TASK_TTL)
+        fttl = self.store.fttl(message.task_id, message.task_id)
+        logger.info(f"FTTL Task_id: {message.task_id} -> {fttl}")
+        return bool(success)
 
     def get(self, task_id: Task_id) -> TaskMessage | None:
         logger.info(f"Get Task_id: {task_id}")
-        message = self.redis.get(task_id)
-        if message.value is None:
+        message = self.store.all(task_id)
+        logger.debug(f"Response TaskMessage: {message}")
+        if not message:
             raise TaskIdNotFoundError(f"Task_id: {task_id} not found")
-        else:
-            value = json.loads(message.value)
-            logger.debug(f"Response TaskMessage: {value}")
-        return TaskMessage(task_id=message.key, **value)
+        return TaskMessage.model_validate(message)
+
+    def list(self) -> list[TaskMessage] | None:
+        messages = self.store.all(self.TASKS_KEY)
+        logger.debug(f"Response TaskMessage: {messages}")
+        if not messages:
+            return []
+        return [self.get(task_id) for task_id in messages]
