045c80a226e6274023d98a2fbfd4deac4fa9fe2c
IX <borntokill.ix@gmail.com>
2025-05-28 21:54:35 +0700

    refactor(app/drivers/ipc/redis/): add forward imports classes

---
+++ app/drivers/ipc/redis/__init__.py
@@ -0,0 +1,15 @@
+from .exceptions import (
+    ConnectionUnavailable,
+    RedisException,
+)
+from .storages import (
+    DeleteStatus,
+    DictStore,
+    HashStore,
+    RedisConnection,
+    RedisMessage,
+    SetStore,
+    SortedSet,
+    SortedSetStore,
+    TTLStatus,
+)
