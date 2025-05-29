c19ee02ed6888477a4a7cf2138a7f79d909381f3
IX <borntokill.ix@gmail.com>
2025-05-29 23:06:47 +0700

    feat(app/api/dependencies/storages.py): add the get_recognition_storage function for getting RedisRecognitionStorage

--- app/api/dependencies/__init__.py
+++ app/api/dependencies/__init__.py
@@ -1,4 +1,4 @@
 from .auth import check_auth
 from .hashers import get_hasher
 from .providers import get_recognizer, get_recognizer_by_task_id, get_recognizers
-from .storages import get_task_storage
+from .storages import get_recognition_storage, get_task_storage
--- app/api/dependencies/storages.py
+++ app/api/dependencies/storages.py
@@ -1,11 +1,23 @@
 from fastapi import HTTPException
 
-from app.adapters.storages.memory import RedisTaskStorage, StorageException
+from app.adapters.storages.memory import (
+    RedisRecognitionStorage,
+    RedisTaskStorage,
+    StorageException,
+)
+from app.interfaces.recognizers import IRecognitionStorage
 from app.interfaces.storages import ITaskStorage
 
 
 def get_task_storage() -> ITaskStorage:
     try:
         return RedisTaskStorage()
+    except StorageException as e:
+        raise HTTPException(status_code=500, detail=e.detail)
+
+
+def get_recognition_storage() -> IRecognitionStorage:
+    try:
+        return RedisRecognitionStorage()
     except StorageException as e:
         raise HTTPException(status_code=500, detail=e.detail)
