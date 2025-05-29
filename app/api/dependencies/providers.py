77b85d920630d20a3cd84aa3e4c739eb4842a289
IX <borntokill.ix@gmail.com>
2025-05-29 23:10:18 +0700

    fix(app/api/dependencies/providers.py): add dependencies for IRecognitionStorage to get_recognizer and get_recognizer_by_task_id functions; pass IRecognitionStorage to constructor IRecognizer via recognizers_fabric

--- app/api/dependencies/providers.py
+++ app/api/dependencies/providers.py
@@ -4,10 +4,11 @@ from fastapi import Depends
 from fastapi.exceptions import HTTPException
 
 from app.adapters.recognizers import Providers
-from app.interfaces.recognizers import IRecognizer, Task_id
+from app.adapters.storages.memory import TaskIdNotFoundError
+from app.interfaces.recognizers import IRecognitionStorage, IRecognizer, Task_id
 from app.interfaces.storages import ITaskStorage
 
-from .storages import get_task_storage
+from .storages import get_recognition_storage, get_task_storage
 
 recognizers_fabric = Providers()
 
@@ -17,11 +18,12 @@ def get_recognizers() -> list[str]:
 
 
 def get_recognizer(
+    storage: Annotated[IRecognitionStorage, Depends(get_recognition_storage)],
     recognizer: Literal[*get_recognizers()],
 ) -> IRecognizer:
     name = recognizer.lower()
     try:
-        return recognizers_fabric[name]
+        return recognizers_fabric[name](storage)
     except AttributeError:
         raise HTTPException(status_code=404, detail=f"No such recognizer {name}")
 
@@ -29,9 +31,12 @@ def get_recognizer(
 def get_recognizer_by_task_id(
     task_id: Task_id,
     storage: Annotated[ITaskStorage, Depends(get_task_storage)],
+    recognizer_storage: Annotated[
+        IRecognitionStorage, Depends(get_recognition_storage)
+    ],
 ) -> IRecognizer:
-    task_message = storage.get(task_id)
     try:
-        return recognizers_fabric[task_message.recognizer]
-    except AttributeError:
+        task_message = storage.get(task_id)
+        return recognizers_fabric[task_message.recognizer](recognizer_storage)
+    except (AttributeError, TaskIdNotFoundError):
         raise HTTPException(status_code=404, detail=f"Task_id {task_id} not found")
