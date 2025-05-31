f16c3eb6996359176a4050ee77348186ce63fd68
IX <borntokill.ix@gmail.com>
2025-05-31 13:47:49 +0700

    refactor(app/drivers/aws/base.py): add to constructor of the S3Client with S3Config configured Protocol implementation

--- app/drivers/aws/base.py
+++ app/drivers/aws/base.py
@@ -1,21 +1,26 @@
-from typing import Optional
+from typing import Optional, Protocol
 
-import boto3
+from boto3 import Session
+
+
+class S3Config(Protocol):
+    __slots__ = ()
+    endpoint_url: str = "http://127.0.0.1:9000"
+    access_key: str
+    secret_key: str
+    session_token: Optional[str] = None
 
 
 class S3Client:
     def __init__(
         self,
-        endpoint_url: str,
-        aws_access_key_id: str,
-        aws_secret_access_key: str,
-        aws_session_token: Optional[str] = None,
+        config: S3Config,
     ):
-        self.session = boto3.session.Session()
+        self.session = Session()
         self.s3_client = self.session.client(
             service_name="s3",
-            aws_access_key_id=aws_access_key_id,
-            aws_secret_access_key=aws_secret_access_key,
-            aws_session_token=aws_session_token,
-            endpoint_url=endpoint_url,
+            aws_access_key_id=config.access_key,
+            aws_secret_access_key=config.secret_key,
+            aws_session_token=config.session_token,
+            endpoint_url=config.endpoint_url,
         )
